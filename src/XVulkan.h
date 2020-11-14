ad8a634d38277e95b4cf3eb30a1541a25ab22b7f
DevJeer <1761569049@qq.com>
2020-11-14 18:43:07 +0800

    第九课：寻找合适的显存空间

--- src/XVulkan.h
+++ src/XVulkan.h
@@ -18,7 +18,7 @@ void xglBufferData(XVulkanHandle vbo, int size, void* data);
 // 创建vbo
 VkResult xGenBuffer(VkBuffer& buffer, VkDeviceMemory& buffermemory, VkDeviceSize size,
 	VkBufferUsageFlags usage, VkMemoryPropertyFlags properties);
-// 
+// 填充fbo
 void xBufferSubData(VkBuffer buffer, VkBufferUsageFlags usage, const void* data,
 	VkDeviceSize size);
 
@@ -28,3 +28,5 @@ void xBufferSubData(VkBuffer buffer, VkBufferUsageFlags usage, const void* data,
 
 #define xBufferSubVertexData(buffer, data, size)\
 	xBufferSubData(buffer, VK_BUFFER_USAGE_TRANSFER_SRC_BIT|VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, data, size)
+// 获取内存的属性
+uint32_t xGetMemoryType(uint32_t type_filters, VkMemoryPropertyFlags properties);
